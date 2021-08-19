d3f2b16059788a508a88a2d5e315dbcb9a7027c2
rgarnaud <garnaud_raphael@outlook.com>
2021-08-19 16:55:12 +0200

    securite

--- Plant-boot/src/main/java/sopra/formation/model/Utilisateur.java
+++ Plant-boot/src/main/java/sopra/formation/model/Utilisateur.java
@@ -35,11 +35,13 @@ public class Utilisateur extends Personne {
 		super();
 	}
 
+
 	public Utilisateur(Long id, int version, String nom, String prenom, String adresseMail, String motDePasse,
-			String pseudo) {
-		super(id, version, nom, prenom, adresseMail, motDePasse, pseudo);
+			String pseudo, boolean enable) {
+		super(id, version, nom, prenom, adresseMail, motDePasse, pseudo, enable);
 		// TODO Auto-generated constructor stub
 	}
+
 
 	public Utilisateur(int version, String numeroTelephone, List<Adresse> adresse, List<Jardin> jardin,
 			List<Devis> devis, List<Recherche> recherche, List<Commentaire> commentaire) {
