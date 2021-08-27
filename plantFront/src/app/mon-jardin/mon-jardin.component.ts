8c075043fcd7626d9b336a1e775ef4bb9a721b1f
rgarnaud <garnaud_raphael@outlook.com>
2021-08-27 17:11:44 +0200

    comit

---
+++ plantFront/src/app/mon-jardin/mon-jardin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mon-jardin',
+  templateUrl: './mon-jardin.component.html',
+  styleUrls: ['./mon-jardin.component.scss']
+})
+export class MonJardinComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
